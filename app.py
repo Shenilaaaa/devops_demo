dab62882171d76635a0b1bfe4a332aa2293e5589
Shenilaaaa <125453736+Shenilaaaa@users.noreply.github.com>
2025-05-16 16:11:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def main():
     print("Hello from Feature Branch! AEHHAGGJG")
     with open("output.txt", "w") as file:
-        file.write("Deployment successful!")
+        file.write("Deployment successful in pycharm project!")
 
 if __name__ == "__main__":
     main()
